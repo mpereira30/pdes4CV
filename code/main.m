b2e5f7f68b8dedf89b7cf4e393d461cf80d11c95
Marcus <mpereira30.markper4190@gmail.com>
2019-04-26 13:10:19 -0400

    undo all parfor crap

--- code/main.m
+++ code/main.m
@@ -53,7 +53,7 @@ max_Iy          = squeeze(max(Iy,[],'all'));
 % Parameters:
 % lambda_s        = [100, 10, 5, 1, 0.5, 0.1, 0.05, 0.01, 0.005, 0.001];
 % lambda_t        = [100, 10, 5, 1, 0.5, 0.1, 0.05, 0.01, 0.005, 0.001];
-lambda_s        = [0.005, 0.001];
+lambda_s        = [0.01, 0.005, 0.001];
 lambda_t        = [100, 10, 5, 1, 0.5, 0.1, 0.05, 0.01, 0.005, 0.001];
 threshold       = 1e-3; % difference threshold for convergence
 
@@ -68,15 +68,10 @@ Iy_sq           = Iy.^2;
 all_rmse_u      = zeros(length(lambda_s),length(lambda_t)); 
 all_rmse_v      = zeros(length(lambda_s),length(lambda_t)); 
 
-num_s = length(lambda_s);
-num_t = length(lambda_t);
-
-% flog = fopen(strcat('log_',category,'.txt'), 'w'); 
-for n_s = 1:num_s
-    lambda_s_curr = lambda_s(n_s);
-    parfor n_t = 1:num_t
+flog = fopen(strcat('log_',category,'.txt'), 'w'); 
+for n_s = 1:length(lambda_s)
+    for n_t = 1:length(lambda_t)
         
-        lambda_t_curr   = lambda_t(n_t);
         % Initialize variables:
         u               = zeros(size(Ix));
         v               = zeros(size(Iy));
@@ -84,15 +79,15 @@ for n_s = 1:num_s
         diff_v          = Inf;
 
         % Step size based on CFL condition:
-        dt_u            = 2 / (max_Ix^2 + 8 * lambda_s_curr + 4 * lambda_t_curr);
-        dt_v            = 2 / (max_Iy^2 + 8 * lambda_s_curr + 4 * lambda_t_curr);
+        dt_u            = 2 / (max_Ix^2 + 8 * lambda_s(n_s) + 4 * lambda_t(n_t));
+        dt_v            = 2 / (max_Iy^2 + 8 * lambda_s(n_s) + 4 * lambda_t(n_t));
 
 %         iter = 1;
         
-%         fprintf(flog, "Params used: n_s=%d/%d, n_t=%d/%d, \nlambda_s: %f\nlambda_t: %f\ndt_u: %e\ndt_v: %e\n\n", ...
-%                 n_s, length(lambda_s), n_t, length(lambda_t),lambda_s(n_s), lambda_t(n_t), dt_u, dt_v);
-%         fprintf("Params used: n_s=%d/%d, n_t=%d/%d, \nlambda_s: %f\nlambda_t: %f\ndt_u: %e\ndt_v: %e\n\n", ...
-%                 n_s, length(lambda_s), n_t, length(lambda_t),lambda_s(n_s), lambda_t(n_t), dt_u, dt_v);
+        fprintf(flog, "Params used: n_s=%d/%d, n_t=%d/%d, \nlambda_s: %f\nlambda_t: %f\ndt_u: %e\ndt_v: %e\n\n", ...
+                n_s, length(lambda_s), n_t, length(lambda_t),lambda_s(n_s), lambda_t(n_t), dt_u, dt_v);
+        fprintf("Params used: n_s=%d/%d, n_t=%d/%d, \nlambda_s: %f\nlambda_t: %f\ndt_u: %e\ndt_v: %e\n\n", ...
+                n_s, length(lambda_s), n_t, length(lambda_t),lambda_s(n_s), lambda_t(n_t), dt_u, dt_v);
 
             while( (diff_u > threshold) || (diff_v > threshold) )
 
@@ -101,9 +96,9 @@ for n_s = 1:num_s
                 u_xx        = [u(:,2:end,:), zeros(nr,1,nf)] + [zeros(nr,1,nf), u(:,1:end-1,:)]; % x-axis is along columns
                 u_yy        = [u(2:end,:,:); zeros(1,nc,nf)] + [zeros(1,nc,nf); u(1:end-1,:,:)]; % y-axis is along rows
                 u_tt        = cat(3,u(:,:,2:end),zeros(nr,nc,1)) + cat(3,zeros(nr,nc,1),u(:,:,1:end-1));
-                u           = (ones_matrix - Ix_sq.*dt_u - 4.*lambda_s_curr.*dt_u - 2.*lambda_t_curr.*dt_u).* u ...
+                u           = (ones_matrix - Ix_sq.*dt_u - 4.*lambda_s(n_s).*dt_u - 2.*lambda_t(n_t).*dt_u).* u ...
                               -(Iy .* Ix .* v + It .* Ix) .* dt_u ... 
-                              + lambda_s_curr .* dt_u .* (u_xx + u_yy) + lambda_t_curr .* dt_u .* u_tt;
+                              + lambda_s(n_s) .* dt_u .* (u_xx + u_yy) + lambda_t(n_t) .* dt_u .* u_tt;
                 diff_u      = max(abs(u - current_u),[],'all'); 
 
                 % Hold u constant and take a "v-step": 
@@ -111,16 +106,16 @@ for n_s = 1:num_s
                 v_xx        = [v(:,2:end,:), zeros(nr,1,nf)] + [zeros(nr,1,nf), v(:,1:end-1,:)]; % x-axis is along columns
                 v_yy        = [v(2:end,:,:); zeros(1,nc,nf)] + [zeros(1,nc,nf); v(1:end-1,:,:)]; % y-axis is along rows
                 v_tt        = cat(3,v(:,:,2:end),zeros(nr,nc,1)) + cat(3,zeros(nr,nc,1),v(:,:,1:end-1));
-                v           = (ones_matrix - Iy_sq.*dt_v -4.*lambda_s_curr.*dt_v - 2.*lambda_t_curr.*dt_v).* v ...
+                v           = (ones_matrix - Iy_sq.*dt_v -4.*lambda_s(n_s).*dt_v - 2.*lambda_t(n_t).*dt_v).* v ...
                               -(Iy .* Ix .* u + It .* Iy) .* dt_v ... 
-                              + lambda_s_curr .* dt_v .* (v_xx + v_yy) + lambda_t_curr .* dt_v .* v_tt;
+                              + lambda_s(n_s) .* dt_v .* (v_xx + v_yy) + lambda_t(n_t) .* dt_v .* v_tt;
                 diff_v      = max(abs(v - current_v),[],'all');     
 
 %                 fprintf("Iteration: %d, u_diff = %f, v_diff = %f\n", iter, diff_u, diff_v);
 %                 iter        = iter +1;  
                 if ((diff_u > 1e3) || (diff_v > 1e3))
-%                     fprintf(flog, "*************Failed for lambda_s=%f, lambda_t=%f ***********\n\n", lambda_s(n_s), lambda_t(n_t));
-                    fprintf("*************Failed for lambda_s=%f, lambda_t=%f ***********\n\n", lambda_s_curr, lambda_t_curr);
+                    fprintf(flog, "*************Failed for lambda_s=%f, lambda_t=%f ***********\n\n", lambda_s(n_s), lambda_t(n_t));
+                    fprintf("*************Failed for lambda_s=%f, lambda_t=%f ***********\n\n", lambda_s(n_s), lambda_t(n_t));
                     break;
                 end
             end
@@ -131,27 +126,25 @@ for n_s = 1:num_s
         maxv = max(v, [], 'all');
         minv = min(v, [], 'all');
 
-%         fprintf(flog, "Flow range for u: %f to %f\n",minu, maxu);
-%         fprintf(flog, "Flow range for v: %f to %f\n",minv, maxv);
+        fprintf(flog, "Flow range for u: %f to %f\n",minu, maxu);
+        fprintf(flog, "Flow range for v: %f to %f\n",minv, maxv);
 
         mag = sqrt(u.^2+v.^2);
         maxmag = max(mag, [], 'all');
 
-%         fprintf(flog, "Max flow magnitude: %f\n",maxmag);
+        fprintf(flog, "Max flow magnitude: %f\n",maxmag);
 
         rmse_u = sqrt(mean((g_u - u(:,:,4)).^2,'all'));
         rmse_v = sqrt(mean((g_v - v(:,:,4)).^2,'all'));
-%         fprintf(flog, "RMSE_u: %f\nRMSE_v: %f\n\n", rmse_u, rmse_v);
-        fprintf("Params used: n_s=%d/%d, n_t=%d/%d, \nlambda_s: %f, lambda_t: %f\ndt_u: %e, dt_v: %e\nRMSE_u: %f, RMSE_v: %f\n\n", ...
-                n_s, num_s, n_t, num_t, lambda_s_curr, lambda_t_curr, dt_u, dt_v, rmse_u, rmse_v);    
+        fprintf(flog, "RMSE_u: %f\nRMSE_v: %f\n\n", rmse_u, rmse_v);
         
-%         all_rmse_u(n_s, n_t) = rmse_u;
-%         all_rmse_v(n_s, n_t) = rmse_v;
+        all_rmse_u(n_s, n_t) = rmse_u;
+        all_rmse_v(n_s, n_t) = rmse_v;
            
     end
 end
-% fclose(flog);
-% save('rmse_errors.mat','all_rmse_u','all_rmse_v')
+fclose(flog);
+save('rmse_errors.mat','all_rmse_u','all_rmse_v')
 
 
 
